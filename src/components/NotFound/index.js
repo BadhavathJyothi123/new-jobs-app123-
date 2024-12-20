c71f2ad3be7c9dda8a5cfa876f58aa00a044fc23
jyothi <jyothi8316@gmail.com>
2024-12-20 10:08:43 +0000

    Solution

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -5,16 +5,14 @@ const NotFound = () => (
   <>
     <Header />
     <div className="not-found-container">
-      <div className="not-found-image-container">
-        <img
-          className="not-found-image"
-          src="https://assets.ccbp.in/frontend/react-js/jobby-app-not-found-img.png"
-          alt="not found"
-        />
-      </div>
-      <h1 className="heading">Page Not Found</h1>
-      <p className="err">
-        we are sorry, the page you requested could not be found
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/jobby-app-not-found-img.png"
+        alt="not found"
+        className="not-found-img"
+      />
+      <h1 className="not-found-heading">Page Not Found</h1>
+      <p className="not-found-description">
+        we’re sorry, the page you requested could not be found
       </p>
     </div>
   </>
